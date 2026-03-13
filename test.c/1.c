f30b71ed50e16eab0976997d8d9bc971521c7c51
18009062817-sketch <18009062817@163.com>
2026-03-13 07:43:33 +0000

    add c program

---
+++ test.c/1.c
@@ -0,0 +1,17 @@
+/*#include<stdio.h>
+int main(){
+    printf("\n12+23=%d\n",12+23);
+    return 0;
+}
+*/
+
+/*#include<stdio.h>
+int main(){
+    int price=0;
+    printf("请输入金额(元):");
+    scanf("%d",&price);
+    int change=100-price;
+    printf("找您%d元\n",change);
+    
+
+}*/
