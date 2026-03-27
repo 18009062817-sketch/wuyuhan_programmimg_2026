7f9f0dea44671cda2f04b404ab03a0d5c277ce61
18009062817-sketch <18009062817@163.com>
2026-03-27 05:09:10 +0000

    add

--- lec3.c/lec3_배열.c
+++ lec3.c/lec3_배열.c
@@ -34,7 +34,7 @@ int main(){
 }
 */
 
-
+/*
 #include<stdio.h>
 #define N 10
 
@@ -46,3 +46,96 @@ int main(){
    
     return 0;
 }
+*/
+
+
+
+
+
+
+/*
+#include<stdio.h>
+
+float average(float input[]);
+
+int main(){
+    float a[5] = {100.0 , 95.0, 93.0, 90.0, 85.0}; 
+    float b[5] = {60.0,65.0,70.0,75.0,80.0};
+    float c[7] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+    
+    int size_a = sizeof(a) / sizeof(a[0]);
+    int size_b = sizeof(b) / sizeof(b[0]);
+    int size_c = sizeof(c) / sizeof(c[0]);
+    float avg = average(a, size_a);
+    float avg_b = average(b, size_b);
+    float avg_c = average(c, size_c);
+
+    printf("Average of array a: %f\n", avg);
+    printf("Average of array b: %f\n", avg_b);
+    printf("Average of array c: %f\n", avg_c);
+
+    return 0;
+}
+
+float average(float input[], int input_num){
+        float sum = 0.0;
+
+        //int num = 5;// 배열의 크기를 고정값으로 설정 (5로 가정)
+        int num = input_num;
+        for (int i = 0; i < num; i++){
+            sum += input[i];
+        }
+        float average = sum / num;
+        
+        return average;
+}
+ */
+ 
+
+/*
+ #include<stdio.h>
+
+int main(){
+    char string[10] = "10element"
+    //printf("%s\n",string);
+
+    for(int i = 0; i < 10; i++){
+        printf("%c\n",string[i]);
+    }
+    printf("\n");
+    return 0;   
+ }
+*/
+
+
+#include<stdio.h>
+#define MAX_SIZE 100
+#define RANGE 100
+
+int main(){
+    int n,i,number;
+    int frequency[RANGE] = {0};
+    int arr[MAX_SIZE];
+
+    printf("Enter the number of elements(max%d):",MAX_SIZE);
+    scanf("%d",&n);
+
+    for(i = 0; i < n; i++){
+        printf("Input %d number:",i+1);
+        scanf("%d",&number);
+        arr[i]=number;
+        if(number>= 0 && number < RANGE){
+            frequency[number]++;
+        }
+
+    }
+    printf("NUmber Frequency\n");
+    for(i = 0; i < RANGE;i++){
+        if(frequency[i] > 0){
+            printf("%d:%d\n",i,frequency[i]);
+        }
+    }
+   
+    return 0;   
+ }
+
